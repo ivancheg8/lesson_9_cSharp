ac738b9ff726bfe63a36ecb3af51c4b7810ec89b
ivancheg8 <pavlevan@yandex.ru>
2022-07-25 18:12:56 +0300

    work done

--- lesson9_c#.cs
+++ lesson9_c#.cs
@@ -4,9 +4,11 @@
 int a = 2;
 int SumOfDigits(int n)
 {
+    if (n < 0) n = n * -1;
+    if (n == 0) return 1;
     if (n >= 10)
     {
-        SumOfDigits( (n / 10));
+        SumOfDigits((n / 10));
         return a++;
     }
     else return 1;
@@ -29,7 +31,7 @@ int ShowNumbs(int n, int m)
     if (n < m)
         return (n + ShowNumbs(n + 1, m));
     if (n > m)
-        return (m + ShowNumbs(n, m+1));
+        return (m + ShowNumbs(n, m + 1));
     else return n;
 }
 Console.Write("Введите первое число: ");
